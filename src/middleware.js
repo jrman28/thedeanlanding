a8699d8ffbe478ac32712474c38ee0bd75bbf599
Justin Rocket <jrman28@gmail.com>
2024-11-04 14:27:04 -0600

    Create middleware.js

---
+++ src/middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: "/integrations/:path*",
+};
+
+export function middleware(request) {
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-createxyz-project-id", "e0321491-b4fa-44f9-80fc-6bac02803b3c");
+
+  request.nextUrl.href = `https://www.create.xyz/${request.nextUrl.pathname}`;
+
+  return NextResponse.rewrite(request.nextUrl, {
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
